04809b2b8cc2c9c632c8856a432f5876dacc8305
dmitrii-shergin <dmitriishergin.nsk@gmail.com>
2024-03-22 15:26:10 +0700

    initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,39 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'airbnb',
+    'airbnb/hooks',
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'plugin:react/jsx-runtime',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    'react/jsx-filename-extension': [
+      'error',
+      { extensions: ['.jsx', '.ts', '.tsx'] },
+    ],
+    'import/no-unresolved': [0],
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        '': 'never',
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+    //
+    'max-len': ['warn', { code: 80 }],
+    'import/prefer-default-export': ['off'],
+    'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+    'no-console': 'off',
+  },
+};
---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { CssBaseline } from '@mui/material';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <CssBaseline />
+    Hello world
+  </React.StrictMode>,
+);
